a367355e975eed108e3fec7357833b7d71440d2e
Tommy Seternus <thomassseternus@gmail.com>
2024-10-12 22:55:56 -0400

    Trying to fix my debugger

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,9 +8,9 @@ namespace ConsoleApp1
 {
     internal class Program
     {
-        static void Main(string[] args
+        static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Is this working?");
         }
     }
 }
